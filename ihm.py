74c673fb43d7854ca8c950aacd6eb722c4a191a6
Da-Lan <dl.nguyen404@gmail.com>
2020-03-29 23:48:41 +0200

    add: functionality Ajouter un produit

--- ihm.py
+++ ihm.py
@@ -44,18 +44,18 @@ def ihm_builder(conn, engine) :
     pages = ['Listes de course', 'Ajouter un produit', 'Visualisations']
     page = st.sidebar.radio("", options=pages)
 
+    sql = "select * from public.produits_a_acheter;"
+    produits_a_acheter = pd.read_sql_query(sql, conn)
+
+    sql = "select * from public.produits_ref;"
+    produits_ref = pd.read_sql_query(sql, conn)
+
+    sql = "select * from public.magasins_ref;"
+    magasins_ref = pd.read_sql_query(sql, conn)
+
 
     ######## Page 1 #######
     if page == pages[0]:
-
-        sql = "select * from public.produits_a_acheter;"
-        produits_a_acheter = pd.read_sql_query(sql, conn)
-
-        sql = "select * from public.produits_ref;"
-        produits_ref = pd.read_sql_query(sql, conn)
-
-        sql = "select * from public.magasins_ref;"
-        magasins_ref = pd.read_sql_query(sql, conn)
 
         # One section for each Magasin
         for index, m_ref in magasins_ref.iterrows():
@@ -101,6 +101,33 @@ def ihm_builder(conn, engine) :
                     produits_a_acheter_maj_details.to_sql('produits_a_acheter', engine, if_exists='append', index=False)
                     
                 st.write('Liste de course mise à jour !')
+
+
+    elif page == pages[1]:
+        st.header("Ajouter des produits à acheter")
+        nom_produit = st.text_input('Nom du produit:')
+        nom_magasin = st.selectbox('Magasin:', magasins_ref['nom'].unique())
+        nom_rayon = st.selectbox('Rayon où trouver le produit:', produits_ref['categorie'].unique())
+        prix = st.number_input('Prix:', value=3.00, step=0.5, min_value=0.00, max_value=1000.00)
+        
+        if nom_produit:
+            if st.button("Enregistrer l'avancement"):
+
+                # insert new products to buy
+                produits_a_ajouter = pd.DataFrame( {
+                    'nom':[nom_produit],
+                    'magasin':[ int(magasins_ref[ magasins_ref['nom'] == nom_magasin]['id']) ],
+                    'categorie':[nom_rayon],
+                    'prix':[prix]
+                } )
+
+                produits_a_ajouter.to_sql('produits_ref', engine, if_exists='append', index=False)
+                
+                st.write('Produit ajouté à la base de donnée !')
+
+        st.subheader("Les 5 derniers produits ajoutés sont:")
+        st.write(produits_ref.tail(5))
+
 
 
 
